21f2fb004d44e6a855c54ccabd248373650c55ae
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-05-22 23:14:57 -0500

    no message

---
+++ src/main/java/edu/mum/ea/utils/SearchProjectParam.java
@@ -0,0 +1,31 @@
+package edu.mum.ea.utils;
+
+public class SearchProjectParam {
+	private String name;
+	private String status;
+	private String location;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
